830c49d129357d52f81a1565f48676309bc4ba52
gmarchena <gmarchena@yahoo.com>
2019-05-10 10:57:00 -0600

    Debug del punto y coma
    Se mantiene el error

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -21,7 +21,7 @@ public class InsultGenerator {
    password);
      
    if (connection != null) {
-   	String SQL = "select"
+   	String SQL = "select";
 	a.string AS first, b.string AS second, c.string AS noun
     from short_adjective a , long_adjective b, noun c ORDER BY random() limit 1";
     Statement stmt = connection.createStatement();
